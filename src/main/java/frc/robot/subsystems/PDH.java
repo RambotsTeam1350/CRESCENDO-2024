aff1111be41ea028683dd7677778fda6ba6d16ba
Myer <myer@myer.wtf>
2024-03-28 09:52:19 -0400

    shuffleboard layout

--- src/main/java/frc/robot/subsystems/PDH.java
+++ src/main/java/frc/robot/subsystems/PDH.java
@@ -3,7 +3,9 @@ package frc.robot.subsystems;
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -16,6 +18,8 @@ public class PDH extends SubsystemBase {
     private final GenericEntry totalCurrentEntry; // amps
     private final GenericEntry totalPowerEntry; // watts
     private final GenericEntry totalEnergyEntry; // joules
+
+    private final ShuffleboardLayout channelsLayout;
     private final GenericEntry channel1currentEntry;
 
     public PDH() {
@@ -27,7 +31,9 @@ public class PDH extends SubsystemBase {
         this.totalCurrentEntry = this.PDHTab.add("Total Current", this.pdh.getTotalCurrent()).getEntry();
         this.totalPowerEntry = this.PDHTab.add("Total Power", this.pdh.getTotalPower()).getEntry();
         this.totalEnergyEntry = this.PDHTab.add("Total Energy", this.pdh.getTotalEnergy()).getEntry();
-        this.channel1currentEntry = this.PDHTab.add("Channel 1 Current", this.pdh.getCurrent(1)).getEntry();
+
+        this.channelsLayout = this.PDHTab.getLayout("Channels", BuiltInLayouts.kList);
+        this.channel1currentEntry = this.channelsLayout.add("Channel 1 Current", this.pdh.getCurrent(1)).getEntry();
     }
 
     @Override
@@ -37,6 +43,7 @@ public class PDH extends SubsystemBase {
         this.totalCurrentEntry.setDouble(this.pdh.getTotalCurrent());
         this.totalPowerEntry.setDouble(this.pdh.getTotalPower());
         this.totalEnergyEntry.setDouble(this.pdh.getTotalEnergy());
+
         this.channel1currentEntry.setDouble(this.pdh.getCurrent(1));
     }
 }
